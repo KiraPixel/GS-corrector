87ee03ef937c8ace30c7d92e888ac83bb8aa6269
Kira <90696175+KiraPixel@users.noreply.github.com>
2023-05-17 23:13:15 +0300

    biggest update

--- sheetsearcher.py
+++ sheetsearcher.py
@@ -1,5 +1,6 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import json
 
 # настройки доступа к таблице
 scope = ['https://spreadsheets.google.com/feeds',
@@ -7,22 +8,30 @@ scope = ['https://spreadsheets.google.com/feeds',
 creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scope)
 client = gspread.authorize(creds)
 
-# открываем таблицу по URL-адресу
-sheet_url = 'https://docs.google.com/spreadsheets/d/1uhEHYOq5Xu66lznlwJ9yVs05nGN664yLGkAX-OHw5XY/edit#gid=0'
-sheet = client.open_by_url(sheet_url)
 
-# задаем необходимые столбы и колонки
-start_row = 2
-column_value = 3
-column_date = 6
+class Placed:
+    def __init__(self, placed_name):
+        with open('config.json', 'r', encoding='utf-8') as file:
+            config_data = json.load(file)
+        data = config_data if isinstance(config_data, dict) else {}
+
+        self.placed_data = data[placed_name]
+        self.start_row = self.placed_data['start_row']
+        self.column_value = self.placed_data['garage_num_row']
+        self.column_date = self.placed_data['date_row']
+        self.sheet_name = self.placed_data['sheet_name']
+        self.sheet_url = self.placed_data['google_sheet_link']
 
 
-def start_search(sheet_name):
-    worksheet = sheet.worksheet(sheet_name)
+def start_search(placed_name):
+    placed_data = Placed(placed_name)
+    # открываем таблицу по URL-адресу
+    sheet = client.open_by_url(placed_data.sheet_url)
+    worksheet = sheet.worksheet(placed_data.sheet_name)
 
     # получаем значения и номера ячеек столбца
-    value_range = worksheet.range(start_row, column_value, worksheet.row_count, column_value)
-    date_range = worksheet.range(start_row, column_date, worksheet.row_count, column_date)
+    value_range = worksheet.range(placed_data.start_row, placed_data.column_value, worksheet.row_count, placed_data.column_value)
+    date_range = worksheet.range(placed_data.start_row, placed_data.column_date, worksheet.row_count, placed_data.column_date)
     cells = [{'value': value_cell.value, 'row': value_cell.row, 'col': value_cell.col,
               'to_date': date_cell.value} for value_cell, date_cell in zip(value_range, date_range)]
 
@@ -30,6 +39,8 @@ def start_search(sheet_name):
 
 
 # корректор дат
-def replace_on_correct(row, new_date,sheet_name):
-    worksheet = sheet.worksheet(sheet_name)
-    worksheet.update_cell(row, column_date, new_date)
+def replace_on_correct(row, new_date, placed_name):
+    placed_data = Placed(placed_name)
+    sheet = client.open_by_url(placed_data.sheet_url)
+    worksheet = sheet.worksheet(placed_data.sheet_name)
+    worksheet.update_cell(row, placed_data.column_date, new_date)
--- starter.py
+++ starter.py
@@ -1,12 +1,13 @@
-import time
-from datetime import datetime
+import json
 import threading
+import time
+
 import gspread
 import tkinter as tk
-from ttkbootstrap import Style
-from ttkbootstrap.widgets import Button
-from ttkbootstrap import ttk
+from datetime import datetime
 
+from ttkbootstrap import Style, ttk
+from ttkbootstrap.widgets import Button
 
 import jirasearcher
 import sheetsearcher
@@ -14,25 +15,21 @@ import sheetsearcher
 
 class ParserGUI:
     def __init__(self):
-
         # Инициализация GUI
-        self.style = Style(theme='darkly')  # Выбор темы darkly (можно выбрать другую тему)
+        self.style = Style(theme='darkly')
         self.window = self.style.master
         self.window.title("Парсер данных")
         self.window.iconbitmap('icon.ico')
-
-        # Настройка размеров и масштабирования окна
-        self.window.geometry("400x450")  # Задайте начальные размеры окна по своему усмотрению
-        self.window.resizable(True, False)  # Разрешить изменение размера окна по обоим осям
+        self.window.geometry("400x450")
+        self.window.resizable(True, False)
 
         # Создание меню
         self.menu_bar = tk.Menu(self.window)
         self.window.config(menu=self.menu_bar)
 
         # Создание текстового поля для лога
-        self.log_text = tk.Text(self.window, height=20)  # Увеличение высоты текстового поля на 400 пикселей
-        self.log_text.pack(fill=tk.BOTH, expand=True)  # Используем fill и expand для масштабирования
-
+        self.log_text = tk.Text(self.window, height=20)
+        self.log_text.pack(fill=tk.BOTH, expand=True)
 
         # Создание скроллбара и привязка его к текстовому полю
         scrollbar = ttk.Scrollbar(self.log_text, style='darkly.Vertical.TScrollbar')
@@ -40,43 +37,56 @@ class ParserGUI:
         self.log_text.config(yscrollcommand=scrollbar.set)
         scrollbar.config(command=self.log_text.yview)
 
-        # Создание кнопки "Начать" и привязка ее к методу toggle_search()
-        self.start_button = Button(
+        # Чтение данных о площадках из config.json
+        with open('config.json', 'r', encoding='utf-8') as file:
+            config_data = json.load(file)
+        placed_data = config_data if isinstance(config_data, dict) else {}
+        # Создание кнопок для каждой площадки и кнопка "Стоп"
+        self.placed_buttons = []
+        for placed_name in placed_data:
+            button_text = placed_name
+            button = Button(
+                self.window,
+                text=button_text,
+                command=lambda placed=placed_name: self.start_search(placed),
+                style='primary.TButton'
+            )
+            button.pack(side='left', padx=5, pady=5)
+            self.placed_buttons.append(button)
+
+        self.stop_button = Button(
             self.window,
-            text="Начать",
-            command=self.toggle_search,
-            style='success.TButton'  # Стиль кнопки успеха (можно выбрать другой стиль)
+            text="Остановить",
+            command=self.stop_search,
+            style='danger.TButton'
         )
-        self.start_button.pack(pady=10)  # Используем pady для отступа
+        self.stop_button.pack(side='left', padx=5, pady=5)
+        self.stop_button.configure(state=tk.DISABLED)
 
         # Инициализация переменных состояния
-        self.is_searching = False  # Флаг поиска данных
-        self.stop_event = threading.Event()  # Событие для остановки поиска
+        self.is_searching = False
+        self.stop_event = threading.Event()
 
-    def toggle_search(self):
-        # Метод для переключения между началом и остановкой поиска
-        if self.is_searching:
-            self.stop_search()  # Если поиск уже выполняется, остановить его
-        else:
-            self.start_search()  # Если поиск не выполняется, начать его
-
-    def start_search(self):
-        # Метод для начала поиска данных
+    def start_search(self, placed_info):
+        # Метод для начала поиска данных для конкретной площадки
         self.is_searching = True
-        self.start_button.config(text="Стоп")  # Изменение надписи кнопки на "Стоп"
-        self.clear_log()  # Очистка лога
+        self.clear_log()
 
-        # Запуск нового потока для выполнения поиска данных
-        t = threading.Thread(target=self.search)
+        for button in self.placed_buttons:
+            button.configure(state=tk.DISABLED)
+        self.stop_button.configure(state=tk.NORMAL)
+
+        t = threading.Thread(target=self.search, args=(placed_info,))
         t.start()
 
     def stop_search(self):
         # Метод для остановки поиска данных
         self.is_searching = False
+        for button in self.placed_buttons:
+            button.configure(state=tk.NORMAL)
+        self.stop_button.configure(state=tk.DISABLED)
         self.log('Закончил работу')
-        self.stop_event.set()  # Установка события для остановки поиска
-        self.start_button.config(text="Начать")  # Изменение надписи кнопки на "Начать"
-
+        self.stop_event.set()
     def clear_log(self):
         # Метод для очистки текстового поля с логом
         self.log_text.delete("1.0", tk.END)
@@ -86,45 +96,38 @@ class ParserGUI:
         self.log_text.insert(tk.END, message + "\n")
         self.log_text.see(tk.END)
 
-    def search(self):
-        # Метод для выполнения поиска данных
-        placed = {"Силикат", "Фрезер"}  # Площадки для парсинга
+    def search(self, placed_name):
+        # Метод для выполнения поиска данных для конкретной площадки
+        self.log(f"Начинаю парсер площадки {placed_name}")
+        cells = sheetsearcher.start_search(placed_name)
 
-        for i in placed:
-            if not self.is_searching:
-                break  # Если поиск был остановлен, выйти из цикла
+        for cell in cells:
+            if self.stop_event.is_set():
+                break
 
-            self.log(f"Начинаю парсер площадки {i}")
-            cells = sheetsearcher.start_search(i)  # Получение ячеек данных
+            if cell['value'] != "":
+                sheets_date = cell['to_date']
 
-            for cell in cells:
-                if self.stop_event.is_set():
-                    break  # Если поиск был остановлен, выйти из цикла
+                jira_date_set = jirasearcher.start_search(cell['value'])  # Поиск даты в Jira
 
-                if cell['value'] != "":
-                    sheets_date = cell['to_date']
+                jira_date_str = next(iter(jira_date_set))
+                jira_date_obj = datetime.strptime(jira_date_str, '%Y-%m-%dT%H:%M:%S.%f%z')
+                formatted_jira_date = jira_date_obj.strftime('%d.%m.%Y')
 
-                    jira_date_set = jirasearcher.start_search(cell['value'])  # Поиск даты в Jira
+                if sheets_date == formatted_jira_date:
+                    # Если дата в Google Sheets совпадает с датой в Jira, записать соответствующее сообщение в лог
+                    self.log(f"{placed_name} | {cell['value']} имеет правильную дату ТО {cell['to_date']}")
+                else:
+                    try:
+                        # Иначе, если даты не совпадают, заменить дату в Google Sheets на дату из Jira
+                        self.log(f"{placed_name} | Меняю дату для {cell['value']}")
+                        sheetsearcher.replace_on_correct(cell['row'], formatted_jira_date, placed_name)
+                    except gspread.exceptions.APIError as e:
+                        # Обработка ошибки API при слишком большом количестве запросов к Google Sheets
+                        self.log(f"Ошибка, слишком много запросов в гугл. Перезапуск через 65 секунд...")
+                        time.sleep(65)
 
-                    jira_date_str = next(iter(jira_date_set))
-                    jira_date_obj = datetime.strptime(jira_date_str, '%Y-%m-%dT%H:%M:%S.%f%z')
-                    formatted_jira_date = jira_date_obj.strftime('%d.%m.%Y')
-
-                    if sheets_date == formatted_jira_date:
-                        # Если дата в Google Sheets совпадает с датой в Jira, записать соответствующее сообщение в лог
-                        self.log(f"{i} | {cell['value']} имеет правильную дату ТО {cell['to_date']}")
-                    else:
-                        try:
-                            # Иначе, если даты не совпадают, заменить дату в Google Sheets на дату из Jira
-                            self.log(f"{i} | Меняю дату для {cell['value']}")
-                            sheetsearcher.replace_on_correct(cell['row'], formatted_jira_date, i)
-                        except gspread.exceptions.APIError as e:
-                            # Обработка ошибки API при слишком большом количестве запросов к Google Sheets
-                            self.log(f"Ошибка, слишком много запросов в гугл. Перезапуск через 65 секунд...")
-                            time.sleep(65)
-
-        self.is_searching = False
-        self.start_button.config(text="Начать")
+        self.stop_search()
         self.stop_event.clear()
 
     def run(self):
@@ -132,7 +135,6 @@ class ParserGUI:
         self.window.mainloop()
 
 
-
-# Создаем экземпляр класса ParserGUI и запускаем при
+# Создаем экземпляр класса ParserGUI и запускаем приложение
 parser_gui = ParserGUI()
-parser_gui.window.mainloop()
+parser_gui.run()
